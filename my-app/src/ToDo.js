e3155660513293bc8997d7407f5583c3ef7d4e12
yablochkov <dima5544@ya.ru>
2022-03-01 14:55:19 +0300

    feat: add simple page layout in ToDo.js

--- my-app/src/ToDo.js
+++ my-app/src/ToDo.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 
 export function ToDo(){
-
+  
 
     return (
         <>
-            
+            <input type="text"  value={item} onInput={(e) => setItem(e.target.value)}/>
+                <button onClick={addTask}>Добавить</button>
+                <ul> 
+                </ul>
         </>
     )
 }
